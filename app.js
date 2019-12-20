257db7844972d34469f355196b212c9b51904976
Mauro Vargas <maurovargas@MacBook-Air-de-Mauro.local>
2019-12-19 21:52:14 -0500

    Fix cors

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ const app = express();
 const {User} = require('./modules');
 const { router } = require('./routes');
 const PORT = process.env.PORT || 4100;
-const cors = require('cors');
 
 app.use(express.urlencoded( {extended: true} ));
 app.use(express.json());
 app.use('/api/v1', router);
-app.use(cors());
 
 app.listen(PORT, (err)=>{
     err ? console.log(err) : console.log(`App running in PORT ${PORT}`);
